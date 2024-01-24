49ffc1eded0a0b210ab1fc1132309514c4a20a10
Dimitar Dimitrov <dtdimitrov12@gmail.com>
2024-01-24 09:58:23 +0200

    Fixed formatting

--- src/test/java/com/example/trainingappointment/TrainingAppointmentApplicationTests.java
+++ src/test/java/com/example/trainingappointment/TrainingAppointmentApplicationTests.java
@@ -4,12 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TrainingAppointmentApplicationTests
-{
+class TrainingAppointmentApplicationTests {
 
     @Test
-    void contextLoads( )
-    {
+    void contextLoads() {
     }
 
 }
